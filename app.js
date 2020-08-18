0e333c3b3daa1efd1d2621d4cb58a19abbacb914
piman <aidenleepi@gmail.com>
2020-08-18 02:27:41 -0400

    Adding minimum required

---
+++ app.js
@@ -0,0 +1,26 @@
+const url = require('url');
+const querystring = require('querystring');
+const express = require('express');
+const unblocker = require('unblocker');
+const Transform = require('stream').Transform;
+
+
+const app = express();
+
+var unblockerConfig = {
+    prefix: '/proxy/'
+};
+
+app.use(unblocker(unblockerConfig));
+
+app.use('/', express.static(__dirname + '/public'));
+
+app.get("/no-js", function(req, res) {
+    var site = querystring.parse(url.parse(req.url).query).url;
+    res.redirect(unblockerConfig.prefix + site);
+});
+
+
+app.listen(Number(process.env.PORT || 8000))
+
+
